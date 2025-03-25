96d56a6bfd4a880e94e771c805e2f14a67ac6b57
Murilo Zamaioli <murilo.zamaioli@gmail.com>
2025-03-24 22:20:05 -0300

    html suits

---
+++ app.js
@@ -0,0 +1,8 @@
+// alert("olá seja bem vindo")
+console.log("posso escrever aqui também")
+console.log(10+2)
+console.log(10-2)
+console.log(10*2)
+console.log(10/5)
+console.log(parseFloat("10") + 2)
+console.log("João" + " " + "Paulo")
